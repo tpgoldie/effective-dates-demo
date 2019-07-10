06e462e8eb7998432f32868d0b497366b7a7de78
Tony Golding <tpg@blueyonder.co.uk>
2019-07-10 10:29:08 +0100

    [TG|RW] Project initialized

---
+++ src/main/java/com/discover/epp/inn/demo/EffectiveDatingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.discover.epp.inn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveDatingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffectiveDatingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/discover/epp/inn/demo/EffectiveDatingDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.discover.epp.inn.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EffectiveDatingDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
